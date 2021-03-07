044f5af0368872439884158160798b3339e4a574
KAN <kansar13@yandex.ru>
2021-03-07 14:04:44 +0400

    "First Blin"

---
+++ Task_1/Task_1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+// Написать программу, которая определяет площадь прямоугольника со сторонами a и b.
+// Если пользователь вводит некорректные знаения (отрицательные или ноль), должно выдаваться сообщение об ошибке.
+//Возможность ввода пользователем строки вида "абвгд" или нецелых чисел игнорировать.
+
+namespace Task_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            do
+            {
+                Console.Write("Введите сторону 'а' (значение больше 0): ");
+                string str_a = Console.ReadLine();
+                a = int.Parse(str_a);
+                if (a < 1)
+                {
+                    Console.WriteLine("Alarm: a<=0");
+                }
+            } while (a < 1);
+            
+
+            Console.Write("Введите сторону b: ");
+            string str_b = Console.ReadLine();
+            //   Console.WriteLine(b);
+            int b = int.Parse(str_b);
+
+            int S = a * b;
+
+            Console.WriteLine("Площадь прямоугольника = {0}", S);
+        }
+    }
+}

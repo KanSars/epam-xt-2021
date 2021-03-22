bbfc54e7ba49a9452dc9b290222f24c002033bf3
Kirill Nahmurin <kansar13@yandex.ru>
2021-03-22 23:31:01 +0400

    Task_2.1.1 Custom_String rewritten

--- Task_2/Task_2.1/Program.cs
+++ Task_2/Task_2.1/Program.cs
@@ -7,8 +7,85 @@ namespace Task_2._1
     {
         static void Main(string[] args)
         {
-            // на реконструкции
+            //Console.WriteLine("Введите строку:");
 
+            string phrase = "asdf";
+            string phrase2 = "asdf";
+            
+            MyString row = new MyString(phrase);
+
+            //проверяем класс и метод: выводит сумму символов массивов, т.к. класс содержит строку как массив
+            char[] s3 = row.ConCat(phrase2);
+            foreach (var item in s3)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class MyString
+    {
+        char[] stringInMassiv;
+
+        public MyString(string phrase)
+        {
+            this.stringInMassiv = new char[phrase.Length];
+            
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                this.stringInMassiv[i] = phrase[i];
+            }
+        }
+        public int CompareLength(string phrase2)
+        {
+            int result;
+
+            if (this.stringInMassiv.Length > phrase2.Length)
+                result = 1;
+            else if (this.stringInMassiv.Length < phrase2.Length)
+                result = -1;
+            else result = 0;
+            return result;
+        }
+        public char[] ConCat(string phrase2)
+        {
+            char[] summOfMassivs;
+            summOfMassivs = new char[stringInMassiv.Length + phrase2.Length];
+
+            for (int i = 0; i <this.stringInMassiv.Length; i++)
+            {
+                summOfMassivs[i] = this.stringInMassiv[i];
+            }
+
+            for (int i = 0; i < phrase2.Length; i++)
+            {
+                summOfMassivs[i + stringInMassiv.Length] = phrase2[i];
+            }
+            return summOfMassivs;
+        }
+
+        public string FindIt(char a)
+        {
+            var result = new StringBuilder();
+            result = null;
+
+            for (int i = 0; i < this.stringInMassiv.Length; i++)
+            {
+                if (stringInMassiv[i] == a)
+                    result.Append(i);
+            }
+            return result.ToString();
+        }
+        public string MyToString()
+        {
+            var result = new StringBuilder();
+            result = null;
+
+            for (int i = 0; i < this.stringInMassiv.Length; i++)
+            {
+                    result.Append(stringInMassiv[i]);
+            }
+            return result.ToString();
         }
     }
 }

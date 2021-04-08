a99dfe912d8726900026fab126a6d10a90f47161
Kirill Nahmurin <kansar13@yandex.ru>
2021-04-08 22:20:48 +0400

    Task_3.1.2 (Program.cs) added

---
+++ Task_3/3.1.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            Console.WriteLine();
+
+            var dict = new Dictionary<string, int>();
+            
+            string[] line;
+            line = Console.ReadLine().ToLower().Split(" ,.?!\'()\"".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach (var word in line)
+            {
+                if (!dict.ContainsKey(word)) dict.Add(word, 1);
+                else dict[word.ToString()] += 1;
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"word= {item.Key} repeated= {item.Value}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Вы в любом случае молодец!");
+        }
+    }
+}
